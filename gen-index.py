066e9f1c7c2576b4f4ffbd95902dcec5802bb777
Jeff Gilbert <jdashg@gmail.com>
2017-09-28 16:19:03 -0700

    os.walk's dirs must be modified in-place to have an effect.

--- gen-index.py
+++ gen-index.py
@@ -57,7 +57,7 @@ body.add("<h1><a href='{}'>Gen-Index:</a>".format(src_url))
 dir_list = body.add('<ul>')
 
 for (cur, dirs, files) in os.walk('./'):
-    dirs = filter(lambda x: x[0] != '.', dirs)
+    dirs[:] = filter(lambda x: x[0] != '.', dirs)
     files = filter(lambda x: x[0] != '.', files)
 
     htmls = filter(lambda x: x.endswith('.html'), files)
