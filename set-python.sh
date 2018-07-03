89a434f18c46d4a30abb90f7457138ea32941a64
Jeff Gilbert <jdashg@gmail.com>
2018-07-02 19:49:48 -0700

    [set-python] Add set-python.sh.

---
+++ set-python.sh
@@ -0,0 +1,5 @@
+# Switch between python2 and python3 easily. (particularly useful for depot_tools on Arch)
+ls -al /usr/bin/python
+sudo ln -s -f /usr/bin/$@ /usr/bin/python
+ls -al /usr/bin/python
+
